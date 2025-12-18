ff14317c3ea4243869063cf7f9ab51976610c113
jlefonde <joris.lefondeur@protonmail.com>
2025-12-18 15:36:40 +0100

    Modified cloudfront s3 access

--- terraform/modules/cdn/main.tf
+++ terraform/modules/cdn/main.tf
@@ -30,7 +30,10 @@ data "aws_iam_policy_document" "cloudfront_s3_access" {
       identifiers = ["cloudfront.amazonaws.com"]
     }
 
-    actions   = ["s3:GetObject"]
+    actions   = [
+      "s3:GetObject",
+      "s3:ListBucket",
+    ]
     resources = ["${aws_s3_bucket.frontend.arn}/*"]
 
     condition {
